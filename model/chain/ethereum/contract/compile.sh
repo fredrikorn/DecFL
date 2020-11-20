f25aaa831f16d6d1ba558406bcd47ec5f7fd6e33
Maja Linderholm <maja.linderholm@gmail.com>
2020-11-20 10:54:01 +0100

    Added removal of files into compile.sh so you don't have to do it manually

--- model/chain/ethereum/contract/compile.sh
+++ model/chain/ethereum/contract/compile.sh
@@ -1,2 +1,6 @@
+#remove files
+rm -rf ./-p/
+rm -rf /tmp/ethbuild
+
 #!/bin/bash
 mkdir /tmp/ethbuild -p && solc --bin --abi Model.sol -o /tmp/ethbuild/ --overwrite && abigen --bin=/tmp/ethbuild/Model.bin --abi=/tmp/ethbuild/Model.abi --pkg=contract --out=Model.go
