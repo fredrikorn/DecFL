015c98cb598f92640be72c5936e02f2a5be22401
Maja Linderholm <maja.linderholm@gmail.com>
2020-12-02 10:42:32 +0100

    Made scatter plots of CPU% and memory usage for all containers. Will now investigate other plot alternatives

--- performance/eval_stats.py
+++ performance/eval_stats.py
@@ -32,8 +32,7 @@ worker3_enh.index = np.arange(1, len(worker3_enh)+1)
 
 cpu_enh = worker1_enh['CPU %'] + worker2_enh['CPU %'] + worker3_enh['CPU %']
 mem_enh = worker1_enh['MEM USAGE / LIMIT'] + worker2_enh['MEM USAGE / LIMIT'] + worker3_enh['MEM USAGE / LIMIT']
-data = {'CPU %': cpu_enh, 'MEM USAGE / LIMIT': mem_enh}
-workers_enh = pd.DataFrame(data=data)
+workers_enh = pd.DataFrame(data={'CPU %': cpu_enh, 'MEM USAGE / LIMIT': mem_enh})
 
 group = df_ori.groupby('NAME')
 redis_ori = group.get_group('redis')
@@ -41,8 +40,6 @@ chain_ori = group.get_group('chain')
 worker1_ori = group.get_group('local_worker1_1')
 worker2_ori = group.get_group('local_worker2_1')
 worker3_ori = group.get_group('local_worker3_1')
-sum_ori = worker1_ori['CPU %'] + worker2_ori['CPU %'] + worker3_ori['CPU %'] 
-workers_ori = sum_ori
 
 worker1_ori.index = np.arange(1, len(worker1_ori)+1)
 worker2_ori.index = np.arange(1, len(worker2_ori)+1)
@@ -50,8 +47,7 @@ worker3_ori.index = np.arange(1, len(worker3_ori)+1)
 
 cpu_ori = worker1_ori['CPU %'] + worker2_ori['CPU %'] + worker3_ori['CPU %']
 mem_ori = worker1_ori['MEM USAGE / LIMIT'] + worker2_ori['MEM USAGE / LIMIT'] + worker3_ori['MEM USAGE / LIMIT']
-data = {'CPU %': cpu_ori, 'MEM USAGE / LIMIT': mem_ori}
-workers_ori = pd.DataFrame(data=data)
+workers_ori = pd.DataFrame(data={'CPU %': cpu_ori, 'MEM USAGE / LIMIT': mem_ori})
 
 ###ADD TIME STAMPS TO THE DATAFRAME###
 redis_enh['TIME'] = np.arange(0,len(redis_enh)/2,0.5)
@@ -86,7 +82,32 @@ ax2 = workers_ori.plot(kind='scatter',x='TIME',y='CPU %',color='blue', label='Or
 red = mpatches.Patch(color='red', label='Enhanced implementation')
 blue = mpatches.Patch(color='blue', label='Original implementation')
 plt.legend(handles=[red, blue])
-plt.title('Workers CPU%')
+plt.title('Sum of Workers CPU%')
 plt.show()
 
+###PLOT MEM FOR REDIS###
+ax1 = redis_enh.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='red', label='Enhanced implementation')
+ax2 = redis_ori.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='blue', label='Original implementation', ax=ax1)
+red = mpatches.Patch(color='red', label='Enhanced implementation')
+blue = mpatches.Patch(color='blue', label='Original implementation')
+plt.legend(handles=[red, blue])
+plt.title('Redis Memory Usage')
+plt.show()
 
+###PLOT MEM FOR CHAIN###
+ax1 = chain_enh.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='red', label='Enhanced implementation')
+ax2 = chain_ori.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='blue', label='Original implementation', ax=ax1)
+red = mpatches.Patch(color='red', label='Enhanced implementation')
+blue = mpatches.Patch(color='blue', label='Original implementation')
+plt.legend(handles=[red, blue])
+plt.title('Chain Memory Usage')
+plt.show()
+
+###PLOT SUM OF CPU% FOR WORKERS###
+ax1 = workers_enh.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='red', label='Enhanced implementation')
+ax2 = workers_ori.plot(kind='scatter',x='TIME',y='MEM USAGE / LIMIT',color='blue', label='Original implementation', ax=ax1)
+red = mpatches.Patch(color='red', label='Enhanced implementation')
+blue = mpatches.Patch(color='blue', label='Original implementation')
+plt.legend(handles=[red, blue])
+plt.title('Sum of Workers Memory Usage')
+plt.show()
